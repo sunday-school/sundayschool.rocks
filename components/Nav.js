a6afdd6e0caf5483ba3a9813a9082a4bf81a0cec
Andrew Monks <a@monks.co>
2016-12-04 04:44:29 -0600

    use external pages arr in nav component

--- components/Nav.js
+++ components/Nav.js
@@ -1,20 +1,23 @@
 import React from 'react'
 import Link from 'next/link'
 
+const pages = [
+  ['/', 'Home'],
+  ['/sessions', 'Sessions']
+]
+
 const Nav = () =>
   <nav>
-    <ol>
+    <ol className='list pl0'>
       {
-        [
-          ['/', 'Home'],
-          ['/sessions', 'Sessions']
-        ].map(([url, title]) => (
+        pages.map(([url, title]) => (
           <li key={title}>
             <Link href={url}><a>{title}</a></Link>
           </li>
         ))
       }
     </ol>
+    <hr />
   </nav>
 
 export default Nav
