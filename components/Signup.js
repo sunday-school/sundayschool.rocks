afa67b2b553c51d3ee1b1e7226dfeb4a84b1924b
Andrew Monks <a@monks.co>
2016-12-04 04:46:41 -0600

    don't use glamour in signup component

--- components/Signup.js
+++ components/Signup.js
@@ -6,29 +6,19 @@ const openWindow = () => {
   return true
 }
 
-const styles = {
-  form: style({
-    border: '1px solid #ccc',
-    padding: 3
-  }),
-  input: style({
-    width: 140
-  })
-}
-
-const Signup = ({className}) =>
+const Signup = () =>
   <form
-    className={styles.form}
+    className='ba bw2 ph2 mw5 mv3'
     action='https://tinyletter.com/sundayschool'
     method='post'
     target='popupwindow'
     onSubmit={openWindow}
   >
     <p>
-      <label for='tlemail'>Enter your email address and I'll email you when we meet up.</label>
+      <label htmlFor='tlemail'>Enter your email address and I'll email you when we meet up.</label>
     </p>
     <p>
-      <input type='text' className={styles.input} name='email' id='tlemail' />
+      <input type='text' name='email' id='tlemail' />
     </p>
     <input type='hidden' value='1' name='embed' />
     <input type='submit' value='Subscribe' />
