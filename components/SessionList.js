ef365fc1f6b3930504dc3179e5f2eef505d4a40e
Andrew Monks <a@monks.co>
2016-12-04 04:46:14 -0600

    use reversiblelist in sessionlist

--- components/SessionList.js
+++ components/SessionList.js
@@ -1,22 +1,26 @@
+import R from 'ramda'
 import React from 'react'
 import Link from 'next/link'
 
+import ReversibleList from './ReversibleList'
 import Page from './Page'
+
+const listItem = date =>
+  <li key={date}>
+    <Link href={`sessions?date=${date}`}><a>{date}</a></Link>
+  </li>
+
+const listSessions = R.pipe(
+  R.keys,
+  R.invoker(0, 'sort'),
+  R.map(listItem)
+)
 
 const SessionList = ({sessions}) =>
   <Page title='All Sessions'>
-    <ul>
-      {
-        Object.keys(sessions)
-          .sort()
-          .reverse()
-          .map(date => (
-            <li>
-              <Link href={`sessions?date=${date}`}><a>{date}</a></Link>
-            </li>
-          ))
-      }
-    </ul>
+    <ReversibleList>
+      {listSessions(sessions)}
+    </ReversibleList>
   </Page>
 
 export default SessionList
