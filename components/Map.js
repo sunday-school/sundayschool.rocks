983ee8a047eca05ed50c6ed05c940abb784f3693
Andrew Monks <a@monks.co>
2016-12-04 04:43:57 -0600

    don't use glamour in map component

--- components/Map.js
+++ components/Map.js
@@ -1,20 +1,12 @@
 import React from 'react'
 import style from 'next/css'
 
-const styles = {
-  iframe: style({
-    border: 0
-  })
-}
-
 const Map = ({location}) =>
   <iframe
-    className={styles.iframe}
-    width='600'
-    height='450'
-    frameborder='0'
+    className='ba0 w-100 h5'
+    frameBorder='0'
     src={`https://www.google.com/maps/embed/v1/place?key=AIzaSyA1KdSgHcworDOmYrubo8zti_e-X4_7ZFU&q=${location.replace(/ /g, '+')}`}
-    allowfullscreen
+    allowFullScreen
   />
 
 export default Map
