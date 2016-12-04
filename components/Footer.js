d6d61891a387438b11fdc5105655b38b392c47ea
Andrew Monks <a@monks.co>
2016-12-04 05:31:45 -0600

    add source code link to footer

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () =>
   <footer>
     <hr />
-    2016
+    2016 | <a href='https://github.com/sunday-school/sundayschool.rocks'>source code</a>
   </footer>
 
 export default Footer
