3c140cfde7e7f9bd017c5096b6fb9476415c5bf1
Andrew Monks <andrew@theuprisingcreative.com>
2016-12-05 00:42:24 -0600

    split footer to corners

--- components/Footer.js
+++ components/Footer.js
@@ -1,9 +1,13 @@
 import React from 'react'
 
 const Footer = () =>
-  <footer>
-    <hr />
-    2016 | <a href='https://github.com/sunday-school/sundayschool.rocks'>source code</a>
+  <footer className='bt pt1 mt4'>
+    <div className='fl w-50'>
+      2016
+    </div>
+    <div className='fl w-50 tr'>
+      <a href='https://github.com/sunday-school/sundayschool.rocks'>source code</a>
+    </div>
   </footer>
 
 export default Footer
