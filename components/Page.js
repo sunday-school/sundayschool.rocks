93b75955471dd3e11e6fd272d2908131069d3ee1
Andrew Monks <andrew@theuprisingcreative.com>
2016-12-05 01:03:52 -0600

    color html, body

--- components/Page.js
+++ components/Page.js
@@ -1,8 +1,14 @@
 import React from 'react'
 import Head from 'next/head'
+import style from 'next/css'
 
 import Header from '../components/Header'
 import Footer from '../components/Footer'
+
+style.global('html, body', {
+  backgroundColor: '#E8FDF5',
+  color: '#5E2CA5'
+})
 
 const Page = ({title, children, noHeader}) =>
   <section className='mw7 center pa4 ba2 sans-serif'>
