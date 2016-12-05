800f01d6e0038697c26d304ce661ab294e3d8819
Andrew Monks <andrew@theuprisingcreative.com>
2016-12-05 00:42:43 -0600

    split header to corners, add church

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import Nav from './Nav'
+import Church from './Church'
 
 const Header = ({children}) =>
-  <header>
-    <h1>{children}</h1>
-    <Nav />
+  <header className='bb cf mb3'>
+    <div className='fl w-10'>
+      <Church />
+    </div>
+    <div className='fl w-60'>
+      <h1 className='pa0 ma0'>{children}</h1>
+    </div>
+    <div className='fl w-30'>
+      <Nav />
+    </div>
   </header>
 
 export default Header
