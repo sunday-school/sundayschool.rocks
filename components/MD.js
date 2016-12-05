48c50b956a02dfa1503f1059e4b93fa00b9c7842
Andrew Monks <andrew@theuprisingcreative.com>
2016-12-05 00:42:56 -0600

    leading on MD component
    should this just be on html?
    
    maybe.
    
    not sure.

--- components/MD.js
+++ components/MD.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactMarkdown from 'react-markdown'
 
-const MD = string => <ReactMarkdown className='measure' source={string} />
+const MD = string => <ReactMarkdown className='lh-copy measure' source={string} />
 
 export default MD
 
