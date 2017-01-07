918e7954ddfc70b464c6608d18c86025005db357
Andrew Monks <a@monks.co>
2017-01-07 17:58:14 -0600

    Create 2016_12_18.js

---
+++ data/sessions/2016_12_18.js
@@ -0,0 +1,90 @@
+export default {
+  date: '2016-12-18',
+  theme: 'React',
+  agenda: `
+- eat pizza
+- make a portfolio website
+
+here are some links we found along the way:
+
+* * *
+
+- http://tachyons.io/#style
+- https://github.com/tachyons-css/tachyons
+
+tachyons is a cool css file // approach that has a declaration for pretty much every possible css thing.
+
+You style your jawns by using class names in your html.
+
+[the components page](http://tachyons.io/components/) is a really good resource for finding out how to css 
+common things.
+
+protip: command-f in the [actual tachyons.css file](https://github.com/tachyons-css/tachyons/blob/master/css/tachyons.css) to look up the class name for a particular css rule.
+
+**tachyons class name autocompletion for sublime text:**
+
+- https://github.com/webchun/tachyons-sublime-autocomplete
+- https://packagecontrol.io/packages/Tachyons%20Autocomplete
+
+* * *
+
+- https://facebook.github.io/react/docs/hello-world.html
+
+react is for making html in javascript.
+
+* * *
+
+- http://standardjs.com/
+
+kinda like spellcheck for javascript. Install a plugin for your text editor.
+
+* * *
+
+- http://webtypography.net/toc/
+
+Whirlwind intro to the important rules of typograhpy, as applied to the web.
+
+* * *
+
+- https://github.com/zeit/next.js/
+
+trendy new javascript framework that makes it pretty easy to get up and running with react.
+
+* * *
+
+- https://help.github.com/articles/associating-text-editors-with-git/
+
+set up git to use sublime text or atom or whatever for commit messages
+
+* * *
+
+- https://css-tricks.com/adding-stroke-to-web-text/
+- https://css-tricks.com/snippets/css/a-guide-to-flexbox/
+
+css tricks is a good resource for "how do I ___ with css"
+
+* * *
+
+- http://www.w3schools.com/TAgs/tryit.asp?filename=tryhtml_link_mailto
+
+email link
+
+* * *
+
+- https://fonts.google.com/
+
+google's huge collection of free fonts for the web
+
+* * *
+
+- http://www.w3schools.com/cssref/css_units.asp
+
+reference on different css units
+
+* * *
+
+- http://netart.rocks/notes/csslayout
+
+nick briz's page on css
+`
+}
