c87e97e997fcb8ead0cc6fdad3f9d7512852ccd0
Andrew Monks <a@monks.co>
2017-01-08 18:22:03 -0600

    add 01/08

---
+++ data/sessions/2017_01_08.js
@@ -0,0 +1,27 @@
+export default {
+  date: '2017-01-08',
+  theme: 'Loose Ends',
+  agenda: `
+## spudging
+
+replacing a wifi jawn in a macbook pro [tutorial here](https://www.ifixit.com/Guide/MacBook+Pro+13-Inch+Unibody+Mid+2012+AirPort-Bluetooth+Board+Replacement/10363)
+
+## getting objs with textures from photoshop into unity
+
+1. in photoshop, extrude your thing
+2. 3d -> generate UVs
+3. (click ok)
+4. under unwrapping options, change it from 'low distortion' to 'fewer seams'
+5. (click ok)
+6. 3d -> export -> 3d layer
+7. choose collada .dae
+8. change the texture format to JPEG
+9. (press ok)
+
+## cuban sandwiches!
+
+## having a website vs. pointing to github (or whatever)
+
+## sweet jam sesh
+`
+}
--- data/sessions/index.js
+++ data/sessions/index.js
@@ -1,5 +1,7 @@
 /* eslint-disable camelcase */
 
+import session_2017_01_08 from './2017_01_08.js'
+import session_2016_12_18 from './2016_12_18.js'
 import session_2016_12_04 from './2016_12_04.js'
 import session_2016_10_23 from './2016_10_23.js'
 import session_2016_11_06 from './2016_11_06.js'
@@ -19,6 +21,8 @@ export default dateize([
   session_2016_11_06,
   session_2016_11_13,
   session_2016_11_20,
-  session_2016_12_04
+  session_2016_12_04,
+  session_2016_12_18,
+  session_2017_01_08
 ])
 
