387293530f6b4b2acd873ffb592076a7d631e312
Andrew Monks <a@monks.co>
2017-01-15 20:06:25 -0600

    add 2017/01/15

---
+++ data/sessions/2017_01_15.js
@@ -0,0 +1,20 @@
+export default {
+  date: '2017-01-15',
+  theme: 'VR, starting something',
+  agenda: `
+## starting a vr-cade
+
+- organizing something big
+- what are the steps
+- making a timeline
+  - see [here](/documents?doc=vr-cade)
+
+## starting an art band
+
+- what are the steps
+- what do you need to decide on beforehand
+- what breaks up bands? hard decisions without clear expectations.
+  - "we got the big show but joey isn't invited: do we say we'll walk without joey or do we go through with it?"
+  - when you're in that situation ^ you want to _already agree_ what the plan is.
+`}
+
--- data/sessions/index.js
+++ data/sessions/index.js
@@ -1,5 +1,6 @@
 /* eslint-disable camelcase */
 
+import session_2017_01_15 from './2017_01_15.js'
 import session_2017_01_08 from './2017_01_08.js'
 import session_2016_12_18 from './2016_12_18.js'
 import session_2016_12_04 from './2016_12_04.js'
@@ -8,21 +9,18 @@ import session_2016_11_06 from './2016_11_06.js'
 import session_2016_11_13 from './2016_11_13.js'
 import session_2016_11_20 from './2016_11_20.js'
 
-const dateize = arr => {
-  const obj = {}
-  arr.forEach(session => {
-    obj[session.date] = session
-  })
-  return obj
-}
+import by from '../../lib/by'
 
-export default dateize([
+const byDate = by('date')
+
+export default byDate([
   session_2016_10_23,
   session_2016_11_06,
   session_2016_11_13,
   session_2016_11_20,
   session_2016_12_04,
   session_2016_12_18,
-  session_2017_01_08
+  session_2017_01_08,
+  session_2017_01_15
 ])
 
