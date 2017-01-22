7686e8bd13465abc16deaaeb31e70199feca10b1
Andrew Monks <a@monks.co>
2017-01-22 17:15:25 -0600

    update year to 2017

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () =>
   <footer className='bt pt1 mt4'>
     <div className='fl w-50'>
-      2016
+      2017
     </div>
     <div className='fl w-50 tr'>
       <a href='https://github.com/sunday-school/sundayschool.rocks'>source code</a>
