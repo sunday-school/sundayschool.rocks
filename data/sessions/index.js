563898d016e699bd581045fdea55186494f1fb85
Andrew Monks <a@monks.co>
2017-01-29 19:01:01 -0600

    date typo

--- data/sessions/index.js
+++ data/sessions/index.js
@@ -1,22 +1,21 @@
 /* eslint-disable camelcase */
 
-import session_2017_01_22 from './2017_01_22.js'
-import session_2017_01_29 from './2017_01_29.js'
-import session_2017_01_15 from './2017_01_15.js'
-import session_2017_01_08 from './2017_01_08.js'
-import session_2016_12_18 from './2016_12_18.js'
-import session_2016_12_04 from './2016_12_04.js'
 import session_2016_10_23 from './2016_10_23.js'
 import session_2016_11_06 from './2016_11_06.js'
 import session_2016_11_13 from './2016_11_13.js'
 import session_2016_11_20 from './2016_11_20.js'
+import session_2016_12_04 from './2016_12_04.js'
+import session_2016_12_18 from './2016_12_18.js'
+import session_2017_01_08 from './2017_01_08.js'
+import session_2017_01_15 from './2017_01_15.js'
+import session_2017_01_22 from './2017_01_22.js'
+import session_2017_01_29 from './2017_01_29.js'
 
 import by from '../../lib/by'
 
 const byDate = by('date')
 
 export default byDate([
-  session_2016_10_29,
   session_2016_10_23,
   session_2016_11_06,
   session_2016_11_13,
@@ -25,6 +24,7 @@ export default byDate([
   session_2016_12_18,
   session_2017_01_08,
   session_2017_01_15,
-  session_2017_01_22
+  session_2017_01_22,
+  session_2017_01_29
 ])
 
