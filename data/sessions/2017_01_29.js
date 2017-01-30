f51f2bd490dc7ee878d01a25d7625ce1b23e03b6
Andrew Monks <a@monks.co>
2017-01-29 18:57:25 -0600

    Create 2017_01_29.js

---
+++ data/sessions/2017_01_29.js
@@ -0,0 +1,27 @@
+export default {
+  date: '2017-01-29',
+  theme: 'Jekyll!',
+  agenda: `
+Today we talked about generating websites with jekyll.
+
+Here are some links!
+
+- https://github.com/amonks/jekyll-template
+
+this is a sorta starter-jekyll template I made. The README.md file has links to some helpful info, and kinda describes what's going on.
+
+- http://jekyllrb.com/docs
+
+jekyll documentation
+
+- https://shopify.github.io/liquid/
+
+- https://prose.io
+
+usable editor for jekyll sites on github
+
+- https://try.github.io
+
+you can do all the jekyll stuff without using git on the command line, but this is a cool starter if you're trying to get into that.
+
+`}
